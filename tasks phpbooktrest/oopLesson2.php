3b726810442888abae914f9691b5e017484cbfcb
vasylshyshola <v,shishola@gmail.com>
2020-03-06 17:01:55 +0200

    complete method of calculating payment of overtime, remain to alter the output names.

--- tasks phpbooktrest/oopLesson2.php
+++ tasks phpbooktrest/oopLesson2.php
@@ -1,4 +1,7 @@
-<?php
+﻿<?php
+
+error_reporting(-1);
+
 class Employee              // employee значит «сотрудник»
 {
     public $name;               // имя-фамилия
@@ -18,8 +21,12 @@ class Employee              // employee значит «сотрудник»
         {
             // Получаем число отработанных часов
             $hours = $this->getTotalHoursWorked();
+            $overTime = $this->getOvertimeHours(); //плучаем сверхурочные что оплачиваюиться в двойе
+            $overTimeSalary = $overTime * ($rate * 2);
             // и умножаем на часовую ставку
+
             $salary = $hours * $this->rate;
+            $salary += $overTimeSalary;
             return $salary;
         }
 
@@ -29,6 +36,27 @@ class Employee              // employee значит «сотрудник»
             $this->name = $name;
             $this->rate = $rate;
         }
+
+        /* public function getShortName(){    //метод не работает
+            $division =[];
+            $division = explode(" ", $this->name);
+            $division[1] = substr($division[1],0,1);
+            $newName = implode(" ", $division);
+            return $division[1];
+
+        } */
+        public function getOvertimeHours(){
+            $overTime = 0;
+            foreach ($this->hours as  $value) {
+                if ($value >= 41) {
+                    $overTime += ($value - 40);
+                }
+            }
+
+            return $overTime;
+        }
+
+
 }
 
 
@@ -43,9 +71,9 @@ $employees = array($ivan, $peter);
 
 
 echo "<table border = 1 >";
-echo "<tr><td>Работник</td><td>Часы</td><td>Ставка</td><td>Зарплаата</tr>";
+echo "<tr><td>Работник</td><td>Часы</td><td>Переработка</td><td>Ставка</td><td>Зарплаата</tr>";
 // Сама таблица
 foreach ($employees as $employee) {
-   echo "<tr><td>{$employee->name}</td> <td>{$employee->getTotalHoursWorked()}</td><td>{$employee->rate}</td> <td>{$employee->getSalary()}</td></tr>";
+   echo "<tr><td>{$employee->name}</td><td>{$employee->getTotalHoursWorked()}</td><td>{$employee->getOvertimeHours()}</td><td>{$employee->rate}</td> <td>{$employee->getSalary()}</td></tr>";
 }
 echo "</table>";
